15454141bf35311e6a94d87afe4a3a58595c9548
Dipin Arora <44615649+dipinarora9@users.noreply.github.com>
2021-08-09 17:02:21 +0530

    optimized code

--- server/functions/index.js
+++ server/functions/index.js
@@ -2,63 +2,55 @@ const functions = require("firebase-functions");
 const firebase = require('firebase-admin');
 let firebaseApp = firebase.initializeApp();
 var db = firebaseApp.firestore();
-/*
-if (location.hostname === "localhost") {
-    db.useEmulator("localhost", 8080);
-}
-*/
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
 exports.pocketMoneyUpdater = functions.https.onRequest(async (request, response) => {
-    // fetch date convert to millisecond epoch 12 AM
-    let today = new Date();
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    let yyyy = today.getFullYear();
-    today = new Date(`${mm}/${dd}/${yyyy} 00:00:00`);
-    let time = today.getTime();
-
+    const time = getTimestamp(new Date());
     // fetch docs of users(parent) with latest renewal date as today 12AM 
-    let map = {};
     let users = await db.collection('users').where('latest_renewal_date', '==', time).get();
-
-    // fetch child of that parent id with renewal time stamp is today 12AM
     users.forEach(async (doc) => {
-        let child = await db.collection('users').doc(doc.id).collection('children').where('pocket_money_details.renewal_date', '==', time).get();
-        let childData = child.docs[0].data();
-        let plan_id = childData.pocket_money_details.pocket_money_plan_id;
+        // fetch child of that parent id with renewal time stamp is today 12AM
+        let children = await db.collection('users').doc(doc.id).collection('children').where('pocket_money_details.renewal_date', '==', time).get();
+        let pocket_money_plans = {};
+        let getMethods = (obj) => Object.getOwnPropertyNames(obj)
+        for (var childDoc in children) {
+            console.log(getMethods(childDoc));
+            let childData = childDoc.data();
+            let plan_id = childData.pocket_money_details.pocket_money_plan_id;
+            console.log("dipin");
+            if (!(plan_id in pocket_money_plans)) {
+                // fetch pocket money plan
+                let pocketMoneyPlans = await db.collection('users').doc(doc.id).collection('pocket_money_plans').where('plan_id', '==', plan_id).get();
+                pocket_money_plans[plan_id] = pocketMoneyPlans.docs[0].data();
+            }
+            const plan = pocket_money_plans[plan_id];
 
-        let newChild = {
-            pocket_money_details: childData.pocket_money_details,
-            username: childData.username,
-            user_id: childData.user_id,
-            balance: childData.balance,
-            renewal_date: childData.pocket_money_details.renewal_date,
-            plan_id: childData.pocket_money_details.pocket_money_plan_id,
-            //parent_id: childData.parent_id,
-            //payment_permission_required: childData.payment_permission_required,
-            //created_date: childData.created_date,
-            //aadhaar_number: childData.aadhaar_number,
-            //is_parent: childData.is_parent,
-            //email: childData.email,
-        };
-        console.log(`plan id: ${plan_id}`);
+            let today = new Date();
+            // add balance update renewal date
+            today.setDate(today.getDate() + plan.recurring_days)
+            childData.balance += plan.amount;
+            childData.pocket_money_details.renewal_date = getTimestamp(today);
 
-        let pocketMoneyPlans = await db.collection('users').doc(doc.id).collection('pocket_money_plans').where('plan_id', '==', plan_id).get();
-        let plan = pocketMoneyPlans.docs[0].data();
-        console.log(plan);
-        let today = new Date();
-
-        newChild.balance += plan.amount;
-        newChild.pocket_money_details.renewal_date = today.setDate(today.getDate() + plan.recurring_days);
-
-        console.log(newChild);
-        await db.collection('users').doc(doc.id).collection('children').doc(child.docs[0].id).set(newChild);
+            await db.collection('users').doc(doc.id).collection('children').doc(childData.user_id).set(childData);
+        }
+        // let childToGetMoneyNow = await db.collection('users').doc(doc.id).collection('children').orderBy('pocket_money_details.renewal_date').limit(1).get();
+        // //1628447400
+        // childToGetMoneyNow = childToGetMoneyNow.docs[0].data();
+        // console.log(childToGetMoneyNow);
+        // const latest_renewal_date = childToGetMoneyNow.pocket_money_details.renewal_date;
+        // console.log(latest_renewal_date);
+        // await db.collection('users').doc(doc.id).update({ "latest_renewal_date": latest_renewal_date });
     });
-
-    // fetch pocket money plan
-    // add balance update renewal date
     functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
+    response.send("Updated");
 });
+
+
+function getTimestamp(date) {
+
+    // fetch date convert to millisecond epoch 12 AM
+    let dd = String(date.getDate()).padStart(2, '0');
+    let mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = date.getFullYear();
+    const d = new Date(`${mm}/${dd}/${yyyy} 00:00:00`);
+    return d.getTime() / 1000;
+}
