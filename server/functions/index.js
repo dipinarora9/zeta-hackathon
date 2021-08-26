c3d83b53aefa345c9ae2bb7e4749cbce8a95054d
prashant_wan <pokerosajwan0@gmail.com>
2021-08-26 23:35:39 +0530

    added parent sign up function

--- server/functions/index.js
+++ server/functions/index.js
@@ -10,6 +10,7 @@ const fusion_obj = {
     IFIID: '140793',
     BUNDLEID: 'd0f32812-d701-4650-945d-f8d8280d621b',
     FUNDACCOUNTID: 'b46779a5-d42d-41b7-ab47-e241c7274a18',
+    AUTH_KEY: 'eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwidGFnIjoiRXNzLU1QWDFKX3Y3VnI4clNTQ0ZEZyIsImFsZyI6IkExMjhHQ01LVyIsIml2IjoicGI3S3Uya2hSbnBJS1hNLSJ9.Af5ZwXx1nAn-Ur2B4Mtixtq9Er1-q8xyqEttJhdpkic.MPvC3iFhy3inzjk5XN7Ksg.QZo4s_G-3JooPrjw3Of95q2AhBzztKBhQoSMPRqXInujrTpYEsI4D0KSvr_mDRhrv6enxcgLO4FerpaPHykBGRM3_nPVIfYqDU7pVV8Lx0w4ZSr4iPx1V7L30x5nXBwC5_P44ERBJfK__0aUEX5EnPil07z8xROmu7sUeGgpcSUPnowIBGeQR0gZKA2HIktYUVaHQwAI3ALIBRJsLYCmKjxeZlxye4PTxTiNSBg70XAo4Mi8tNsfxfLIhHll5ExuZX4BxfdkUVblKPbSVXGs3Fbi49AdNxhOaC8QTLCaXy48Kzz9UxB8yjtw1q6-IXYt1plRDJaqWGCaEaacW_RZmHHvZQgYJVL5WT92sBftsHZWuKVv1vu5XmF539H2UvNf.SzLl7CbL7_uhdy6mNz4xjw',
 }
 // var messaging = admin.messaging();
 
@@ -96,11 +97,91 @@ function getCurrentServerTime() {
     });
 });*/
 
+function createNewIndividual(data) {
+    let res = await new Promise(async (resolve, reject) => {
+        try {
+            let response = await fetch(`${fusion_obj.BASE_URL}/api/v1/ifi/${fusion_obj.IFIID}/applications/newIndividual`, {
+                method: 'POST',
+                headers: {
+                    'X-Zeta-AuthToken': fusion_obj.AUTH_KEY,
+                    'Cache-Control': 'no-cache'
+                },
+                body: data,
+            });
+            if (response.status !== 200) {
+                reject({ 'status': 'FAIL' });
+            }
+            let data = await response.json();
+            resolve({
+                'status': 'SUCCESS',
+                'individualID': data.individualID
+            });
+        } catch (err) {
+            console.log('error creating new individual here');
+            console.log(err.message);
+            reject({ 'status': 'FAIL' });
+        }
+    })
+    return res;
+}
+
+function issueBundle(bundleName, accountHolderID, email) {
+    let res = await new Promise((resolve, reject) => {
+        try {
+            let formData = {
+                "accountHolderID": accountHolderID,
+                "name": bundleName,
+                "email": email
+            }
+            let issueResponse = await fetch(`${fusion_obj.BASE_URL}/api/v1/ifi/${fusion_obj.IFIID}/bundles/${fusion_obj.BUNDLEID}/issueBundle`, {
+                method: 'POST',
+                headers: {
+                    'X-Zeta-AuthToken': fusion_obj.AUTH_KEY,
+                    'Content-Type': 'application/json'
+                },
+                body: formData,
+            });
+            if (issueResponse.status !== 200) {
+                reject({ 'status': 'FAIL' });
+            } else {
+                let response = await issueResponse.json();
+                resolve({
+                    'status': 'SUCCESS',
+                    'accountID': response.accounts[0].accountID,
+                    'resourceID': response.paymentInstruments[0].resourceID,
+                });
+            }
+        } catch (err) {
+            reject({
+                'status': 'FAIL',
+                'message': err.message
+            });
+        }
+    });
+}
+
 exports.ParentSignUp = functions.https.onRequest(async (request, response) => {
     // create acc holder
-    // issue bundle -> name: pool account / personal account
+    // issue bundle -> name: parent account
     // pool account create
-    // 
+    let res = new Promise((resolve, reject) => {
+        try {
+            let params = request.body;
+            params.ifiID = fusion_obj.IFIID;
+            params.individualType = 'REAL';
+            params.applicationType = "CREATE_ACCOUNT_HOLDER";
+            // params.source = 'postman';
+            let data = createNewIndividual(params);
+            if (data.status === 'FAIL') {
+                reject({ 'status': 'FAIL' });
+            } else {
+                // issue bundle -> bundle name, id, email
+                let accountPaymentProducts = issueBundle('parent account', data.individualID, data.vectors.value);
+            }
+        } catch (err) {
+            reject();
+        }
+    })
 });
 
 exports.ChildSignUp = functions.https.onRequest(async (request, response) => {
@@ -110,6 +191,49 @@ exports.ChildSignUp = functions.https.onRequest(async (request, response) => {
     // 
 });
 
+function getAccountIDViaAccountHolderID(accountHolderID) {
+    return new Promise(async (resolve, reject) => {
+        let accountIDResponse = await fetch(`${fusion_obj.BASE_URL}/api/v1/ifi/${fusion_obj.IFIID}/individuals/${accountHolderID}/accounts`, {
+            method: 'GET',
+            headers: {
+                'X-Zeta-AuthToken': fusion_obj.AUTH_KEY
+            },
+        })
+        let responseObj = await accountHolderID.json();
+        if (accountIDResponse.status !== 200) {
+            reject('');
+        }
+        resolve(responseObj.id);
+    });
+}
+
+function getAccountIDsViaEmail(email) {
+    let res = await new Promise(async (resolve, reject) => {
+        try {
+            let accountHolderIDResponse = await fetch(`${fusion_obj.BASE_URL}/api/v1/ifi/${fusion_obj.IFIID}/individualByVector/e/${email}`, {
+                method: 'GET',
+                headers: {
+                    'X-Zeta-AuthToken': fusion_obj.AUTH_KEY
+                }
+            });
+            let status = accountHolderIDResponse.status;
+            let accountHolderID = await accountHolderIDResponse.json();
+            if (status !== 200) {
+                // error
+                reject('');
+            }
+            let response = await getAccountIDViaAccountHolderID(accountHolderID);
+            if (response === '') reject('');
+            else resolve(response);
+        } catch (err) {
+            // error
+            reject('');
+        }
+    });
+    console.log(res);
+    return res;
+}
+
 exports.A2ATransaction = functions.https.onRequest(async (request, response) => {
     // fetch sender reciver accountID using email using fusion API, 
     // SUCCESS in response
@@ -117,51 +241,38 @@ exports.A2ATransaction = functions.https.onRequest(async (request, response) => 
     let res = await new Promise(async (resolve, reject) => {
         try {
             let params = request.body;
-            let email = params.email;
+            let creaditUserEmail = params.email1;
+            let debitUserEmail = params.email2;
             let amount = params.amount;
-            console.log(email, ' ', amount);
+            console.log(creaditUserEmail, ' ', debitUserEmail, ' ', amount);
             let creditAccountID = '398c1a31-8fe0-4ebb-81ab-70a91c158dee';
             let debitAccountID = 'e45f4149-2ff0-4ec7-89fa-0248724c3277';
-            // let formData = {
-            //     "requestID": uuidv4(),
-            //     "amount": {
-            //         "amount": amount,
-            //         "currency": "INR",
-            //     },
-            //     "transferCode": "ATLAS_P2M_AUTH",
-            //     "debitAccountID": debitAccountID,
-            //     "creditAccountID": creditAccountID,
-            //     "remarks": "TEST",
-            //     "transferTime": getCurrentServerTime(), // time
-            //     "attributes": {}
-            // }
+            // let creditAccountID = getAccountIDsViaEmail(creaditUserEmail);
+            // let debitAccountID = getAccountIDsViaEmail(debitUserEmail);
             let formData = {
-                "requestID": "19",
+                "requestID": uuidv4(),
                 "amount": {
+                    "amount": amount,
                     "currency": "INR",
-                    "amount": 1
                 },
                 "transferCode": "ATLAS_P2M_AUTH",
-                "debitAccountID": "398c1a31-8fe0-4ebb-81ab-70a91c158dee",
-                "creditAccountID": "e45f4149-2ff0-4ec7-89fa-0248724c3277",
-                "transferTime": 1574741608000,
-                "remarks": "DIPIN TO SHAHINA",
+                "debitAccountID": debitAccountID,
+                "creditAccountID": creditAccountID,
+                "remarks": "TEST",
+                "transferTime": getCurrentServerTime(), // time
                 "attributes": {}
             }
-            // console.log(formData);
-            // let { creditAccountID, debitAccountID } = getAccountIDsViaEmail;
-            let responseTransaction = await fetch(`https://fusion.preprod.zeta.in/api/v1/ifi/${fusion_obj.IFIID}/transfers`, {
+            let responseTransaction = await fetch(`${fusion_obj.BASE_URL}/api/v1/ifi/${fusion_obj.IFIID}/transfers`, {
                 method: 'POST',
                 headers: {
                     "Content-Type": "application/json",
                     'X-Zeta-AuthToken': fusion_obj.AUTH_KEY
                 },
-                body: formData,
+                body: JSON.stringify(formData),
             });
             let status = responseTransaction.status;
             console.log(status);
             let response = await responseTransaction.json();
-            console.log('response');
             console.log(response);
             if (status !== 200) {
                 reject({
@@ -183,9 +294,8 @@ exports.A2ATransaction = functions.https.onRequest(async (request, response) => 
             });
         }
     });
-    //     status: 'success' / 'fail'
     console.log(res);
-    resolve.send(res);
+    response.send(res);
     // return res;
 });
 
